41beb1165d32f644ccdbe7a7493450ef8370660b
vas <vasiliy.babenko@gmail.com>
2026-05-14 00:11:20 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	botToken := "8669066608:AAHQeaPBVCT_khKKTWSEZsDXWe7pAWjuoMo"
+
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	updates := bot.GetUpdatesChan(tgbotapi.NewUpdate(0))
+
+	for update := range updates {
+		if update.Message != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+			log.Printf("Echo: %s", update.Message.Text)
+		}
+	}
+}
