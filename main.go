c830c2f58ac3033a68192418d5311b38f576ba49
vas <vasiliy.babenko@gmail.com>
2026-05-14 19:09:23 +0700

    add minimax AI on server

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var gameState = make(map[int64]bool)
+type GameState struct {
+	Board   []string `json:"board"`
+	Xmoves  []int    `json:"xMoves"`
+	Omoves  []int    `json:"oMoves"`
+	LastMove int     `json:"lastMove"`
+}
+
+var gameState = make(map[int64]*GameState)
+var botToken = "8669066608:AAHQeaPBVCT_khKKTWSEZsDXWe7pAWjuoMo"
+var webAppURL = "https://BabenkoVasiliy.github.io/"
 
 func main() {
-	botToken := "8669066608:AAHQeaPBVCT_khKKTWSEZsDXWe7pAWjuoMo"
-
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Panic(err)
@@ -18,14 +28,19 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	http.HandleFunc("/move", handleMove)
+
+	go func() {
+		log.Println("HTTP server started on 0.0.0.0:8080")
+		http.ListenAndServe("0.0.0.0:8080", nil)
+	}()
+
 	kb := tgbotapi.NewReplyKeyboard(
 		tgbotapi.NewKeyboardButtonRow(
 			tgbotapi.NewKeyboardButton("Найти соперника"),
 			tgbotapi.NewKeyboardButton("Играть с ботом"),
 		),
 	)
-
-	webAppURL := "https://BabenkoVasiliy.github.io/"
 
 	updates := bot.GetUpdatesChan(tgbotapi.NewUpdate(0))
 
@@ -35,7 +50,11 @@ func main() {
 			text := update.Message.Text
 
 			if text == "Играть с ботом" {
-				gameState[chatID] = true
+				gameState[chatID] = &GameState{
+					Board:  make([]string, 9),
+					Xmoves: []int{},
+					Omoves: []int{},
+				}
 
 				btn := tgbotapi.NewInlineKeyboardButtonURL("🎮 Играть", webAppURL)
 				kbInline := tgbotapi.NewInlineKeyboardMarkup(tgbotapi.NewInlineKeyboardRow(btn))
@@ -56,8 +75,134 @@ func main() {
 		if update.CallbackQuery != nil {
 			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
 			bot.Request(callback)
-
-			log.Printf("Callback from %d: %s", update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
 		}
 	}
 }
+
+type MoveRequest struct {
+	ChatID   int64   `json:"chatId"`
+	Board    []string `json:"board"`
+	Xmoves   []int   `json:"xMoves"`
+	Omoves   []int   `json:"oMoves"`
+	LastMove int     `json:"lastMove"`
+}
+
+type MoveResponse struct {
+	Index    int    `json:"index"`
+	Remove   int    `json:"remove"`
+}
+
+func handleMove(w http.ResponseWriter, r *http.Request) {
+	var req MoveRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	move := calculateBotMove(req.Board, req.Xmoves, req.Omoves)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(move)
+}
+
+func calculateBotMove(board []string, xMoves, oMoves []int) MoveResponse {
+	emptyCells := []int{}
+	for i, v := range board {
+		if v == "" {
+			emptyCells = append(emptyCells, i)
+		}
+	}
+
+	if len(emptyCells) == 0 {
+		return MoveResponse{Index: -1}
+	}
+
+	bestMove := -1
+	bestScore := -1000
+
+	for _, idx := range emptyCells {
+		board[idx] = "O"
+		testMoves := append(oMoves, idx)
+		score := minimax(board, testMoves, xMoves, 0, false)
+		board[idx] = ""
+
+		if score > bestScore {
+			bestScore = score
+			bestMove = idx
+		}
+	}
+
+	removeIdx := -1
+	if len(oMoves) >= 3 {
+		removeIdx = oMoves[0]
+	}
+
+	return MoveResponse{Index: bestMove, Remove: removeIdx}
+}
+
+func minimax(board []string, oMoves, xMoves []int, depth int, isMaximizing bool) int {
+	winner := checkWinnerBot(board, oMoves, xMoves)
+	if winner == "O" {
+		return 100 - depth
+	}
+	if winner == "X" {
+		return depth - 100
+	}
+	if isDrawBot(board) {
+		return 0
+	}
+
+	if isMaximizing {
+		maxScore := -1000
+		for i := 0; i < 9; i++ {
+			if board[i] == "" {
+				board[i] = "O"
+				score := minimax(board, append(oMoves, i), xMoves, depth+1, false)
+				board[i] = ""
+				if score > maxScore {
+					maxScore = score
+				}
+			}
+		}
+		return maxScore
+	} else {
+		minScore := 1000
+		for i := 0; i < 9; i++ {
+			if board[i] == "" {
+				board[i] = "X"
+				score := minimax(board, oMoves, append(xMoves, i), depth+1, true)
+				board[i] = ""
+				if score < minScore {
+					minScore = score
+				}
+			}
+		}
+		return minScore
+	}
+}
+
+func checkWinnerBot(board []string, oMoves, xMoves []int) string {
+	lines := [][]int{
+		{0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+		{0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+		{0, 4, 8}, {2, 4, 6},
+	}
+
+	for _, line := range lines {
+		if board[line[0]] != "" && board[line[0]] == board[line[1]] && board[line[0]] == board[line[2]] {
+			return board[line[0]]
+		}
+	}
+	return ""
+}
+
+func isDrawBot(board []string) bool {
+	for _, v := range board {
+		if v == "" {
+			return false
+		}
+	}
+	return true
+}
+
+var _ = strconv.Atoi
